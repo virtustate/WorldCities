2330db2a19543490e5697c1c964a04c5d9101aea
Stephen Kelley <stevek@virtustate.com>
2020-04-18 16:56:07 -0500

    fetching and displaying data done

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CitiesComponent } from './cities/cities.component';
+import { CountriesComponent } from './countries/countries.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material.module';
 
@@ -17,6 +18,7 @@ import { AngularMaterialModule } from './angular-material.module';
     NavMenuComponent,
     HomeComponent,
     CitiesComponent,
+    CountriesComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -25,6 +27,7 @@ import { AngularMaterialModule } from './angular-material.module';
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'cities', component: CitiesComponent },
+      { path: 'countries', component: CountriesComponent },
     ]),
     BrowserAnimationsModule,
     AngularMaterialModule
--- ClientApp/src/app/countries/countries.component.ts
+++ ClientApp/src/app/countries/countries.component.ts
@@ -4,16 +4,16 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 
-import { City } from './city';
+import { Country } from './country';
 
 @Component({
-  selector: 'app-cities',
-  templateUrl: './cities.component.html',
-  styleUrls: ['./cities.component.css']
+  selector: 'app-countries',
+  templateUrl: './countries.component.html',
+  styleUrls: ['./countries.component.css']
 })
-export class CitiesComponent {
-  public displayedColumns: string[] = ['id', 'name', 'lat', 'lon'];
-  public cities: MatTableDataSource<City>;
+export class CountriesComponent {
+  public displayedColumns: string[] = ['id', 'name', 'iso2', 'iso3'];
+  public countries: MatTableDataSource<Country>;
 
   defaultPageIndex: number = 0;
   defaultPageSize: number = 10;
@@ -46,7 +46,7 @@ export class CitiesComponent {
   }
 
   getData(event: PageEvent) {
-    var url = this.baseUrl + 'api/Cities';
+    var url = this.baseUrl + 'api/Countries';
     var params = new HttpParams()
       .set("pageIndex", event.pageIndex.toString())
       .set("pageSize", event.pageSize.toString())
@@ -68,7 +68,7 @@ export class CitiesComponent {
         this.paginator.length = result.totalCount;
         this.paginator.pageIndex = result.pageIndex;
         this.paginator.pageSize = result.pageSize;
-        this.cities = new MatTableDataSource<City>(result.data);
+        this.countries = new MatTableDataSource<Country>(result.data);
       }, error => console.error(error));
   }
 }
