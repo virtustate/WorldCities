6addcec6b4f11ebe06418e11afcea2b14174356e
Stephen Kelley <stevek@virtustate.com>
2020-04-20 15:34:36 -0500

    finished chapter 6

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HomeComponent } from './home/home.component';
 import { CitiesComponent } from './cities/cities.component';
 import { CityEditComponent } from './cities/city-edit.component';
 import { CountriesComponent } from './countries/countries.component';
+import { CountryEditComponent } from './countries/country-edit.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material.module';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
@@ -22,6 +23,7 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
     CitiesComponent,
     CityEditComponent,
     CountriesComponent,
+    CountryEditComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -33,6 +35,8 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
       { path: 'city/:id', component: CityEditComponent },
       { path: 'city', component: CityEditComponent },
       { path: 'countries', component: CountriesComponent },
+      { path: 'country/:id', component: CountryEditComponent },
+      { path: 'country', component: CountryEditComponent },
     ]),
     BrowserAnimationsModule,
     AngularMaterialModule,
--- Controllers/CountriesController.cs
+++ Controllers/CountriesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WorldCities.Data;
 using WorldCities.Data.Models;
+using System.Linq.Dynamic.Core;
 
 namespace WorldCities.Controllers
 {
@@ -118,6 +119,35 @@ namespace WorldCities.Controllers
         private bool CountryExists(int id)
         {
             return _context.Countries.Any(e => e.Id == id);
+        }
+
+        [HttpPost]
+        [Route("IsDupeField")]
+        public bool IsDupeField(int countryId, string fieldName, string fieldValue)
+        {
+            /*
+            switch(fieldName)
+            { 
+            case "name":
+                return _context.Countries.Any(
+                    c => c.Name == fieldValue && c.Id != countryId);
+            case "iso2":
+                return _context.Countries.Any(
+                    c => c.ISO2 == fieldValue && c.Id != countryId);
+            case "iso3":
+                return _context.Countries.Any(
+                    c => c.ISO3 == fieldValue && c.Id != countryId);
+                default:
+            return false;
+            }
+            */
+            // Alternative approach (using System.Linq.Dynamic.Core)
+            return (ApiResult<Country>.IsValidProperty(fieldName, true))
+                ? _context.Countries.Any(
+                    String.Format("{0} == @0 && Id != @1", fieldName),
+                    fieldValue,
+                    countryId)
+                : false;
         }
     }
 }
